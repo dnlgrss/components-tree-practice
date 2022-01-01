0e6d7f1ffb4d1d196495ecd01b9c3fc0a19ffef2
dnlgrs <64085659+dnlgrss@users.noreply.github.com>
2022-01-01 20:03:56 +0100

    Initial commit

---
+++ src/components/InputArea.jsx
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+
+function InputArea(props) {
+  const [inputText, setInputText] = useState("");
+
+  function handleChange(event) {
+    const newValue = event.target.value;
+    setInputText(newValue);
+  }
+
+  return (
+    <div className="form">
+      <input onChange={handleChange} type="text" value={inputText} />
+      <button
+        onClick={() => {
+          //Only way of passing arguments through components is calling a props inside a function
+          props.addItem(inputText);
+          setInputText("");
+        }}
+      >
+        <span>Add</span>
+      </button>
+    </div>
+  );
+}
+
+export default InputArea;
